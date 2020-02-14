80b722350e10dcf72961d07ec1f9889930b0498d
Linus <justwitti@gmail.com>
2020-02-14 19:11:19 +0100

    add flag stuff

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"flag"
 	"github.com/The-Penguin-Circle/chat-backend/questions"
 	"github.com/The-Penguin-Circle/chat-backend/sockets"
 	"net/http"
+	"strconv"
 
 	"log"
 )
@@ -18,7 +20,9 @@ func errorClosure(toCall func(w http.ResponseWriter, r *http.Request) error) fun
 }
 
 func main() {
+	port := flag.Int("port", 8080, "the port the server runs on")
+	flag.Parse()
 	http.HandleFunc("/get-questions", errorClosure(questions.ServeQuestions))
 	http.HandleFunc("/websocket", errorClosure(sockets.WebSocket))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(*port), nil))
 }
