5c466835f29ed2d0667333252c17bfebf060043f
Simon Eßlinger <simon@esslinger.it>
2020-02-14 19:29:15 +0100

    feat: implement GenerateImage()

--- generation/generation.go
+++ generation/generation.go
@@ -2,12 +2,29 @@
 package generation
 
 import (
+	"encoding/base64"
+	"io/ioutil"
+	"log"
 	"math/rand"
+	"net/http"
+	"strconv"
 	"strings"
 	"time"
 )
 
 var random = rand.New(rand.NewSource(time.Now().Unix()))
+var defaultImageBase64 string
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+
+	content, err := ioutil.ReadFile("files/defaultImage.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defaultImageBase64 = string(content)
+}
 
 // GenerateUsername returns a username.
 func GenerateUsername() string {
@@ -52,5 +69,19 @@ func getRandomConsonant() string {
 
 // GenerateImage returns an image as base64.
 func GenerateImage() (string, error) {
-	return "", nil
+
+	resp, err := http.Get("https://robohash.org/" + strconv.Itoa(random.Int()))
+	if err != nil {
+		return defaultImageBase64, nil
+	}
+
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return defaultImageBase64, nil
+	}
+
+	return base64.StdEncoding.EncodeToString(data), nil
 }
